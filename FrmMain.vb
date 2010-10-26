01acd53f9c35f1602779b6a11ed3fde1d6348ce2
Shad Miller <sdm12@sage.southhills.edu>
2010-10-26 15:51:07 -0400

    first commit

---
+++ FrmMain.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.Win32
+Public Class FrmMain
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Try
+
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings", "ProxyEnable", 0)
+
+        Catch ex As Exception
+            MessageBox.Show("Error occurred")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Try
+
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings", "ProxyEnable", 1)
+
+        Catch ex As Exception
+            MessageBox.Show("Error occurred")
+
+        End Try
+
+    End Sub
+End Class
